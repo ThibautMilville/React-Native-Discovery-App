7a29d5ab1638ef04fa199abc040cf04f66f7089f
ThibautMilville <tmilville.pro@gmail.com>
2024-03-04 16:00:52 +0100

    Display OS + version

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, Alert } from 'react-native';
+import { SafeAreaView, Alert, Text, Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Child } from './components/Child/Child';
 
@@ -9,13 +9,14 @@ export default function App() {
 
 	return (
 		<SafeAreaProvider>
-			<SafeAreaView style={{ backgroundColor: 'red', flex: 1 }}>
+			<SafeAreaView style={{ backgroundColor: 'green', flex: 1 }}>
 				{/* 				
         <View style={{ backgroundColor: 'blue', flex: 1 }}></View>
         <View style={{ backgroundColor: 'white', flex: 1 }}></View>
         <View style={{ backgroundColor: 'red', flex: 1 }}></View>
         */}
 				<Child Action={sayHello} />
+        <Text style={{textAlign: 'center', color: 'white'}}>{(Platform.OS === 'android' ? 'Running on Android' : 'Running on iOS') + ' v.' + Platform.Version}</Text>
 			</SafeAreaView>
 		</SafeAreaProvider>
 	);
